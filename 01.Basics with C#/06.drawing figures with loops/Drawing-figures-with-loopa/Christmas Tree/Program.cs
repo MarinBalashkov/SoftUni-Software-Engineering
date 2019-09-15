acfee33db4eb7bd45fd0acc1624bd99ae1c87ce9
MarinBalashkov <Mkbalashkov@students.softuni.bg>
2019-09-15 17:06:02 +0300

    drawing with loop 2, 3

---
+++ 01.Basics with C#/06.drawing figures with loops/Drawing-figures-with-loopa/Christmas Tree/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Christmas_Tree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+
+            string mid = " | ";
+
+            for (int row = 0; row <= size; row++)
+            {
+                
+
+                for (int space = 0; space < size - row ; space++)
+                {
+                    Console.Write(" ");
+                }
+                for (int star = 0; star < row; star++)
+                {
+                    Console.Write("*");
+                }
+                Console.Write(mid);
+                for (int star = 0; star < row; star++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();  
+            }
+
+            for (int i = 0; i <=size; i++)
+            {
+                string spacess = new string(' ', size - i);
+                string starss = new string('*', i);
+
+                string rowsss = string.Format("{0}{1}{2}{1}", spacess, starss, mid);
+                Console.WriteLine(rowsss);
+
+
+            }
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ 01.Basics with C#/06.drawing figures with loops/Drawing-figures-with-loopa/House/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace House
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string baseStars = new string('*', n - 2);
+            string baseRow = $"|{baseStars}|";
+
+
+
+            int starsCount = 1;
+            if (n % 2 == 0)
+            {
+                starsCount = 2;
+            }
+
+            for (int i = 0; i < (n + 1) / 2; i++)
+            {
+                string roofStars = new string('*', starsCount);
+                int dashCont = (n - starsCount) / 2;
+                string dashes = new string('-', dashCont);
+                string row = dashes + roofStars + dashes;
+                Console.WriteLine(row);
+                starsCount += 2;
+
+            }
+
+
+            for (int i = 0; i < n/2; i++)
+            {
+                Console.WriteLine(baseRow);
+            }
+        }
+    }
+}
