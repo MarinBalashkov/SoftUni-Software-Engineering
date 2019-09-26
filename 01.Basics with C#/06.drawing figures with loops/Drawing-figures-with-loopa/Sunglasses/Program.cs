caec1a6c4eb9d970c9c37b0e3873562c8c9baee3
MarinBalashkov <Mkbalashkov@students.softuni.bg>
2019-09-26 17:07:36 +0300

    programming basic files

---
+++ 01.Basics with C#/06.drawing figures with loops/Drawing-figures-with-loopa/Sunglasses/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sunglasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+
+            string stars = new string('*', size * 2);
+            string middlespaces = new string(' ', size);
+            string bridge = new string('|', size);
+            string lens = new string('/',2 *size - 2 );
+            string border = stars + middlespaces + stars;
+            string inside = $"*{lens}*"; 
+
+            Console.WriteLine(border);
+            for (int i = 1; i <= size - 2; i++)
+            {
+                if (!(i == (size -1) / 2))
+                {
+                    Console.WriteLine(inside + middlespaces +inside);
+                }
+                else
+                {
+                    Console.WriteLine(inside + bridge + inside);
+                }
+            }
+            Console.WriteLine(border);
+        }
+    }
+}
