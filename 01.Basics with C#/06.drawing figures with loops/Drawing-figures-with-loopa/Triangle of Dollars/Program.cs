06b417a4165d419c2e1ad7772cd99aa25201b2c2
MarinBalashkov <Mkbalashkov@students.softuni.bg>
2019-09-26 17:07:47 +0300

    programming basic files

---
+++ 01.Basics with C#/06.drawing figures with loops/Drawing-figures-with-loopa/Triangle of Dollars/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangle_of_Dollars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("$ ");
+                }
+                Console.WriteLine("$");
+            }
+            for (int i = n; i >= 1; i--)
+            {
+                for (int j = 1; j <= i - 1; j++)
+                {
+                    Console.Write("$ ");
+                }
+                Console.WriteLine("$");
+            }
+
+
+        }
+    }
+}
