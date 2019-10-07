992759b619c7a5cfb43472d51cfe8c6589217be6
MarinBalashkov <Mkbalashkov@students.softuni.bg>
2019-10-07 17:57:16 +0300

    add Data Types files

---
+++ 02.TechnologyFundamentals/02.DATA TYPES AND VARIABLES/DATA TYPES AND VARIABLES/Data Type Finder/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Data_Type_Finder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string line = Console.ReadLine();
+
+                if (line == "END")
+                {
+                    break;
+                }
+
+                bool isBoolean = bool.TryParse(line, out bool boolean);
+                bool isInteger = int.TryParse(line, out int integer );
+                bool isCharacter = char.TryParse(line, out char character);
+
+                if (isBoolean)
+                {
+                    Console.WriteLine($"{boolean}is boolean type");
+                }
+
+
+
+                //.........
+                //........
+            }
+        }
+    }
+}
