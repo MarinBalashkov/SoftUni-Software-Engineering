c503b705082901d85826d095613dea57cbc77abe
MarinBalashkov <Mkbalashkov@students.softuni.bg>
2019-10-12 18:10:01 +0300

    add Arrays files

---
+++ 02.TechnologyFundamentals/03.ARRAYS/ARRAYS/04.Array Rotation/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.Array_Rotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int firstNumber = numbers[0];
+
+                for (int j = 0; j <numbers.Length -1; j++)
+                {
+                    numbers[j] = numbers[j + 1];
+                }
+
+                numbers[numbers.Length - 1] = firstNumber;
+
+            }
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write($"{numbers[i]} ");
+            }
+            Console.WriteLine(string.Join(" ", numbers));
+
+
+        }
+    }
+}
---
+++ 02.TechnologyFundamentals/03.ARRAYS/ARRAYS/05.Top Integers/Program.cs
@@ -0,0 +1,48 @@
+﻿
+
+namespace _05.Top_Integers
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public class Program
+    {
+       public static void Main()
+        {
+            
+            
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            for (int i = 0; i < numbers.Length; i++)
+
+            {
+                int num1 = numbers[i];
+
+                for (int j = i +1; j < numbers.Length; j++)
+                {
+                    int num2 = numbers[j];
+
+
+                    if (num1 <= num2)
+                    {
+                        break;
+                    }
+                    else if (j == numbers.Length -1)
+                    {
+                        Console.Write(num1 + " ");
+                    }
+                }
+
+            }
+            Console.Write(numbers[numbers.Length -1]);
+
+
+
+
+        }
+    }
+}
