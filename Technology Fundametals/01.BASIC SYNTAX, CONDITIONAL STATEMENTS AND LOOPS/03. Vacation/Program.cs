cbac4f2ee6e58a9e17efc1487651a38518bf75f7
MarinBalashkov <dokera2@gmail.com>
2019-10-14 15:45:05 +0300

    Problems 3 and 4

---
+++ Technology Fundametals/01.BASIC SYNTAX, CONDITIONAL STATEMENTS AND LOOPS/03. Vacation/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+
+namespace _03._Vacation
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int NumberOfPeople = int.Parse(Console.ReadLine());
+            string GroupType = Console.ReadLine().ToLower();
+            string Day = Console.ReadLine().ToLower();
+
+            double TotalPrice = 0;
+
+            if (Day == "friday")
+            {
+                if (GroupType == "students")
+                {
+                    if (NumberOfPeople >= 30)
+                    {
+                        TotalPrice = (NumberOfPeople*8.45)-((NumberOfPeople * 8.45)*0.15);
+                    }
+                    else
+                    {
+                        TotalPrice = (NumberOfPeople * 8.45);
+                    }
+
+                }
+                else if (GroupType == "business")
+                {
+                    if (NumberOfPeople >= 100)
+                    {
+                        TotalPrice = (NumberOfPeople - 10) * 10.90;
+                    }
+                    else
+                    {
+                        TotalPrice = NumberOfPeople * 10.90;       
+                    }
+
+                }
+                else if (GroupType == "regular")
+                {
+                    if (NumberOfPeople >= 10 && NumberOfPeople <= 20)
+                    {
+                       TotalPrice = (NumberOfPeople * 15) - ((NumberOfPeople * 15) * 0.15);
+                    }
+                    else
+                    {
+                        TotalPrice = NumberOfPeople * 15;
+                    }
+
+                }
+
+            }
+            else if (Day == "saturday")
+            {
+                if (GroupType == "students")
+                {
+                    if (NumberOfPeople >= 30)
+                    {
+                        TotalPrice = (NumberOfPeople * 9.80) - ((NumberOfPeople * 9.80) * 0.05);
+                    }
+                    else
+                    {
+                        TotalPrice = (NumberOfPeople * 9.80);
+                    }
+
+                }
+                else if (GroupType == "business")
+                {
+                    if (NumberOfPeople >= 100)
+                    {
+                        TotalPrice = (NumberOfPeople - 10) * 15.60;
+                    }
+                    else
+                    {
+                        TotalPrice = NumberOfPeople * 15.60;
+                    }
+
+                }
+                else if (GroupType == "regular")
+                {
+                    if (NumberOfPeople >= 10 && NumberOfPeople <= 20)
+                    {
+                        TotalPrice = (NumberOfPeople * 20) - ((NumberOfPeople * 20) * 0.05);
+                    }
+                    else
+                    {
+                        TotalPrice = NumberOfPeople * 20;
+                    }
+
+                }
+
+
+            }
+            else if (Day == "sunday")
+            {
+                if (GroupType == "students")
+                {
+                    if (NumberOfPeople >= 30)
+                    {
+                        TotalPrice = (NumberOfPeople * 10.46) - ((NumberOfPeople * 10.46) * 0.15);
+                    }
+                    else
+                    {
+                        TotalPrice = (NumberOfPeople * 10.46);
+                    }
+
+                }
+                else if (GroupType == "business")
+                {
+                    if (NumberOfPeople >= 100)
+                    {
+                        TotalPrice = (NumberOfPeople - 10) * 16;
+                    }
+                    else
+                    {
+                        TotalPrice = NumberOfPeople * 16;
+                    }
+
+                }
+                else if (GroupType == "regular")
+                {
+                    if (NumberOfPeople >= 10 && NumberOfPeople <= 20)
+                    {
+                        TotalPrice = (NumberOfPeople * 22.50) - ((NumberOfPeople * 22.50) * 0.05);
+                    }
+                    else
+                    {
+                        TotalPrice = NumberOfPeople * 22.50;
+                    }
+
+                }
+
+            }
+
+
+            Console.WriteLine($"Total price: {TotalPrice:f2}");
+
+
+        }
+    }
+}
---
+++ Technology Fundametals/01.BASIC SYNTAX, CONDITIONAL STATEMENTS AND LOOPS/04. Print and sum/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _04._Print_and_sum
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int StartNum = int.Parse(Console.ReadLine());
+            int EndtNum = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+
+            for (int i = StartNum; i <= EndtNum; i++)
+            {
+                Console.Write($"{i} ");
+                sum += i;
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Sum: {sum}");
+
+
+            
+
+        }
+
+    }
+}
