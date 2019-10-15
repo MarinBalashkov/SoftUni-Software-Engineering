3e92e7f3a966c9dc33fc27ef1c3c8aa4c8c0f41d
MarinBalashkov <dokera2@gmail.com>
2019-10-15 14:51:18 +0300

    Problem 6

---
+++ Technology Fundametals/01.BASIC SYNTAX, CONDITIONAL STATEMENTS AND LOOPS/06.Strong Number/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _06.Strong_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int originalNumber = number;
+
+            int digit = 0;
+            int sumFact = 0;
+
+            while (number != 0)
+            {
+                digit = number % 10;
+                number /= 10;
+
+                int fact = 1;
+                for (int i = 1; i <= digit; i++)
+                {
+                    fact *= i;
+                }
+                sumFact += fact;
+            }
+
+            if (originalNumber == sumFact)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
