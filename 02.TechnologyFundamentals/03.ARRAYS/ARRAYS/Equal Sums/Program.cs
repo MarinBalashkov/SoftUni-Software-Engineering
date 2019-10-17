147534ec105fe2729c1fd3bd1ce7987378af91f1
MarinBalashkov <Mkbalashkov@students.softuni.bg>
2019-10-17 18:10:54 +0300

    add Arrays files

---
+++ 02.TechnologyFundamentals/03.ARRAYS/ARRAYS/Equal Sums/Program.cs
@@ -0,0 +1,73 @@
+﻿
+
+namespace Equal_Sums
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+   public class Program
+    {
+       public static void Main()
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToList();
+
+            bool pint = false;
+            int print = 0;
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                int leftSum = 0;
+                int rightSum = 0;
+
+                if (i == 0)
+                {
+                    leftSum = 0;
+                    for (int j = i +1; j < numbers.Count; j++)
+                    {
+                        rightSum += numbers[j];
+                    }
+
+                }
+                else if (i == numbers.Count - 1)
+                {
+                    rightSum = 0;
+                    for (int j = 0; j < i; j++)
+                    {
+                        leftSum += numbers[j];
+                    }
+                }
+                else
+                {
+                    for (int j = 0; j < i; j++)
+                    {
+                        leftSum += numbers[j];
+                    }
+                    for (int j = i + 1; j < numbers.Count; j++)
+                    {
+                        rightSum += numbers[j];
+                    }
+
+                }
+                if (leftSum == rightSum)
+                {
+                    print = i;
+                    pint = true;
+                }   
+            }
+
+
+            if (pint == true)
+            {
+                Console.WriteLine(print);
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
---
+++ 02.TechnologyFundamentals/03.ARRAYS/ARRAYS/Kamino Factory/Program.cs
@@ -0,0 +1,102 @@
+﻿
+
+namespace Kamino_Factory
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int maxSeqLEngth = int.MinValue;
+            int minSeqStart = int.MinValue;
+            int maxSum = int.MinValue;
+
+            List<int> result = new List<int>();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "Clone them!")
+                {
+                    break;
+                }
+
+                List<int> currenDNA = input
+                    .Split('!', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)                    
+                    .ToList();
+
+                int currenSum = 0;
+
+                for (int i = 0; i < currenDNA.Count; i++)
+                {
+                    int currentNum = currenDNA[i];
+                    currenSum += currentNum;
+                }
+
+                int longestSeqLength = 1;
+                int longestSeqStart = 0;
+
+                int currentSeqLength = 1;
+                int currentSeqStart = 0;
+                
+
+                for (int i = 1; i < currenDNA.Count; i++)
+                {
+                    int firstNumber = currenDNA[i - 1];
+                    int secondNumber = currenDNA[i];
+                    if (firstNumber ==1 && secondNumber == 1)
+                    {
+                        currentSeqLength++;
+
+                        if (currentSeqLength > longestSeqLength)
+                        {
+                            longestSeqLength = currentSeqLength;
+                            longestSeqStart = currentSeqStart;
+                        }
+                    }
+                    else
+                    {
+                        currentSeqLength = 1;
+                        currentSeqStart = i;
+                    }
+                }
+               
+
+                if (longestSeqLength > maxSeqLEngth)
+                {
+                    maxSeqLEngth = longestSeqLength;
+                    result = currenDNA;
+
+                }
+                else if (longestSeqLength == maxSeqLEngth)
+                {
+                    if (longestSeqStart < minSeqStart)
+                    {
+                        minSeqStart = longestSeqStart;
+                        result = currenDNA;
+                    }
+                    else if (longestSeqStart == minSeqStart)
+                    {
+                        if (currenSum > maxSum)
+                        {
+                            maxSum = currenSum;
+                            result = currenDNA;
+                        }
+                    }
+                }
+
+
+
+
+            }
+
+            Console.WriteLine(string.Join('!', result));
+        }
+    }
+}
