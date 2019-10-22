25c63fe597f797ad1560875df76785cab37aab1a
MarinBalashkov <Mkbalashkov@students.softuni.bg>
2019-10-22 18:34:56 +0300

    add Methods files

---
+++ 02.TechnologyFundamentals/04.METHODS  FUNCTIONS/METHODS  FUNCTIONS/leture/Program.cs
@@ -0,0 +1,45 @@
+﻿
+
+namespace leture
+{
+    using System;
+    using System.Text;
+    using System.Diagnostics;
+    public class Program
+    {
+
+
+        public static void Main()
+        {
+            string some = "text" + true + 15;
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            sw.Stop();
+            sw.Elapsed();
+
+             StringBuilder result = new StringBuilder();
+
+
+            for (int i = 0; i < 100000; i++)
+            {
+                result.Append(i);
+            }
+            Console.WriteLine(result.ToString());
+
+           
+            
+
+            //string result = string.Empty;
+
+            //for (int i = 0; i < 100000; i++)
+            //{
+            //    result += i.ToString();
+            //}
+            //Console.WriteLine(result);
+
+        }
+
+       
+
+    }
+}
