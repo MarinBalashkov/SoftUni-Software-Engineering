2362fb40b9d9b661ffa01d27ae63c4daba352f1a
MarinBalashkov <Mkbalashkov@students.softuni.bg>
2019-11-02 19:15:09 +0200

    add Objects-and-Classes files

---
+++ 02.TechnologyFundamentals/07.OBJECTS AND CLASSES/07. Objects-and-Classes-Exercise/01.Advertisement Message/Program.cs
@@ -0,0 +1,60 @@
+﻿
+
+namespace _01.Advertisement_Message
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            var phrases = new List<string> {"Excellent product."
+                , "Such a great product."
+                , "I always use that product."
+                , "Best product of its category."
+                , "Exceptional product."
+                , "I can’t live without this product."};
+
+            var events = new List<string> { "Now I feel good."
+                , "I have succeeded with this product."
+                , "Makes miracles. I am happy of the results!"
+                , "I cannot believe but now I feel awesome."
+                , "Try it yourself, I am very satisfied."
+                , "I feel great!" };
+
+            var authors = new List<string> { "Diana"
+                , "Petya"
+                , "Stella"
+                , "Elena"
+                , "Katya"
+                , "Iva"
+                , "Annie"
+                , "Eva" };
+
+            var cities = new List<string> { "Burgas"
+                , "Sofia"
+                , "Plovdiv"
+                , "Varna"
+                , "Ruse" };
+
+            var random = new Random();
+
+            for (int i = 0; i < n; i++)
+            {
+                int randomPhrases = random.Next(0, phrases.Count);
+                int randomEvents = random.Next(0, events.Count);
+                int randomAuthors = random.Next(0, authors.Count);
+                int randomCities = random.Next(0, cities.Count);
+
+                Console.WriteLine($"{phrases[randomPhrases]}{events[randomEvents]}{authors[randomAuthors]}-{cities[randomCities]}");
+            }
+       
+
+        }
+
+    }
+}
---
+++ 02.TechnologyFundamentals/07.OBJECTS AND CLASSES/07. Objects-and-Classes-Exercise/02.Articles/Program.cs
@@ -0,0 +1,83 @@
+﻿
+
+namespace _02.Articles
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Article
+    {
+        public Article(string title, string content, string author)
+        {
+            
+            this.Title = title;
+            this.Content = content;
+            this.Author = author;
+
+        }
+
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
+
+        public void Edit(string elements)
+        {
+            this.Content = elements;
+        }
+
+        public void ChangeAuthor(string elements)
+        {
+            this.Author = elements;
+        }
+
+        public void Rename(string elements)
+        {
+            this.Title = elements;
+
+        }
+
+        public override string ToString()
+        {
+            return $"{Title} - {Content}: {Author}";
+        }
+
+
+    }
+    public class Program
+    {
+        public static void Main()
+        {
+            List<string> input = Console.ReadLine().Split(", ").ToList();
+            string title = input[0];
+            string content = input[1];
+            string author = input[2];
+
+            var article = new Article(title, content, author);
+            
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                List<string> commands = Console.ReadLine().Split(": ").ToList();
+                string command = commands[0];
+                string elements = commands[1];
+
+                if (command == "Edit")
+                {
+                    article.Edit(elements);
+                }
+                else if (command == "ChangeAuthor")
+                {
+                    article.ChangeAuthor(elements);
+                }
+                else if (command == "Rename")
+                {
+                    article.Rename(elements);
+                }
+            }
+            Console.WriteLine(article);
+           
+        }
+    }
+}
