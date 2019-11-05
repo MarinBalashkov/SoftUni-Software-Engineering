51f1fe3f489b347fdbcbae21630a56107f0f18d5
MarinBalashkov <Mkbalashkov@students.softuni.bg>
2019-11-05 19:16:18 +0200

    add Objects-and-Classes files

---
+++ 02.TechnologyFundamentals/07.OBJECTS AND CLASSES/OBJECTS AND CLASSES LAB/ConsoleApp5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    public class Cat
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Color { get; set; }
+
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var cat = new Cat
+            {
+                Name = "Ivan",
+                Age = 5
+
+
+            };
+            ChangeName(cat);
+
+            Console.WriteLine(cat.Name);
+
+        }
+        public static void ChangeName(Cat cat)
+        {
+            cat.Name = "Pesho";
+        }
+
+    }
+}
---
+++ 02.TechnologyFundamentals/07.OBJECTS AND CLASSES/OBJECTS AND CLASSES LAB/ConsoleApp6/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp6
+{
+    public class Cat
+    {
+        public Cat(string name)
+        {
+            ValidateCatName(name);
+
+            
+            this.Name = name;
+        }
+        public Cat(string name, int age)
+        {
+            ValidateCatName(name);
+            if (age > 0)
+            {
+                throw new Exception("Cat age must be more than 0.");
+            }
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Color { get; set; }
+
+        void ValidateCatName(string name)
+        {
+            if (name == null || name.Length < 2)
+            {
+                throw new Exception("Cat name is not valid. It should be more than 2 symbols long.");
+            }
+
+        }
+
+    }
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var cat = new Cat("Ivan");
+
+            
+            var newCat = new Cat("Pesho", 7);
+
+        }
+    }
+}
