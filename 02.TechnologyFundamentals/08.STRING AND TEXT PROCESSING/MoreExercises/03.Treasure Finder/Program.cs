869d4a5adbfcb20bfcc0f17557e399655b1dd93d
MarinBalashkov <Mkbalashkov@students.softuni.bg>
2019-11-12 19:33:06 +0200

    add String Lecture files

---
+++ 02.TechnologyFundamentals/08.STRING AND TEXT PROCESSING/MoreExercises/03.Treasure Finder/Program.cs
@@ -0,0 +1,62 @@
+﻿
+
+namespace _03.Treasure_Finder
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            List<int> key = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            
+
+            while (true)
+            {
+                string inputLine = Console.ReadLine();
+
+                if (inputLine == "find")
+                {
+                    break;
+                }
+
+                string result = string.Empty;
+
+
+                for (int i = 0; i < inputLine.Length; i++)
+                {
+                    if (i == key.Count)
+                    {
+                        inputLine = inputLine.Remove(0, key.Count);
+                        i = -1;
+                        continue;
+
+                    }
+
+                    int currentChar = inputLine[i];
+                    int decrease = key[i];
+
+                    result += (char)(currentChar - decrease);
+
+                    
+                }
+
+                int typeStartIndex = result.IndexOf('&') +1;
+                int typeEndIndex = result.LastIndexOf('&');
+                string type = result.Substring(typeStartIndex, typeEndIndex - typeStartIndex);
+
+                int cordinateStarIndex = result.IndexOf('<') +1;
+                int cordinateEndIndex = result.LastIndexOf('>');
+                string cordinate = result.Substring(cordinateStarIndex, cordinateEndIndex - cordinateStarIndex);
+
+                Console.WriteLine($"Found {type} at {cordinate}");
+            }
+        }
+    }
+}
---
+++ 02.TechnologyFundamentals/08.STRING AND TEXT PROCESSING/MoreExercises/04.Morse Code Translator/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.Morse_Code_Translator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string morseCode = Console.ReadLine();
+            
+
+            morseCode = morseCode             
+                .Replace("-...", "B")
+                .Replace("-.-.", "C")             
+                .Replace("..-.", "F")              
+                .Replace("....", "H")               
+                .Replace(".---", "J")               
+                .Replace(".-..", "L")                               
+                .Replace(".--.", "P")
+                .Replace("--.-", "Q")               
+                .Replace("...-", "V")                
+                .Replace("-..-", "X")
+                .Replace("-.--", "Y")
+                .Replace("--..", "Z")
+                .Replace(".-.", "R")
+                .Replace("...", "S")
+                .Replace("..-", "U")
+                .Replace("---", "O")
+                .Replace("-.-", "K")
+                .Replace("--.", "G")
+                .Replace("-..", "D")
+                .Replace(".--", "W")
+                .Replace("..", "I")
+                .Replace("--", "M")
+                .Replace("-.", "N")
+                .Replace(".-", "A")
+                .Replace(".", "E")
+                .Replace("-", "T")
+                .Replace(" ", string.Empty)
+                .Replace("|", " ");
+            Console.WriteLine(morseCode);
+                
+
+        }
+    }
+}
