d8529b6d45c607fbb379eda1ba7e3604e215b9cb
MarinBalashkov <Mkbalashkov@students.softuni.bg>
2019-11-26 19:43:34 +0200

    add some files

---
+++ 02.TechnologyFundamentals/11.WEB PROJECT/Forum - Skeleton/Forum/Models/Category.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Forum.Models
+{
+    public class Category
+    {
+        public Category()
+        {
+            Topics = new List<Topic>();
+        }
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = " Category Name")]
+        public string Name { get; set; }
+
+        
+        public string AuthorId { get; set; }
+        public ApplicationUser Author { get; set; }
+
+
+        public List<Topic> Topics { get; set; }
+
+        [NotMapped]
+        public int NumberTopics => Topics.Count;
+
+    }
+}
