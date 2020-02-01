c6a91fd0f8c11ce3813a57845afed32f1bd976dd
MarinBalashkov <Mkbalashkov@students.softuni.bg>
2020-02-01 13:55:06 +0200

    add c#Advance folder, Stacks and Queues folder and Problem 1 from exercises !!

---
+++ 03.C# Advanced/01.STACKS AND QUEUES/Exercises/Stacks and Queues exercises/01. Basic Stack Operations/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _01._Basic_Stack_Operations
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<int> list = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            int num1 = list[0];
+            int num2 = list[1];
+            int num3 = list[2];
+
+            Stack<int> source = new Stack<int>(Console.ReadLine().Split().Select(int.Parse).ToList());
+
+            for (int index = 0; index < num2; ++index)
+            {
+                source.Pop();
+            }
+
+            if (source.Contains(num3))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine(source.Min());
+            }
+        }
+    }
+}
