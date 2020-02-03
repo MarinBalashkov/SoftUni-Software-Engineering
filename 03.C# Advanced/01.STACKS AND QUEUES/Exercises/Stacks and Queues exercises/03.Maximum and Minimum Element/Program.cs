1a50c170eee087fe4817586df4be56753e8bebb4
MarinBalashkov <Mkbalashkov@students.softuni.bg>
2020-02-03 15:57:06 +0200

    add Problem 3

---
+++ 03.C# Advanced/01.STACKS AND QUEUES/Exercises/Stacks and Queues exercises/03.Maximum and Minimum Element/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace _03.Maximum_and_Minimum_Element
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            int queries = int.Parse(Console.ReadLine());
+            Stack<int> source = new Stack<int>();
+
+            for (int index = 0; index < queries; ++index)
+            {
+                var list = Console.ReadLine().Split().Select(int.Parse).ToList();
+                int command = list[0];
+
+                switch (command)
+                {
+                    case 1:
+                        int pushedhNumber = list[1];
+                        source.Push(pushedhNumber);
+                        continue;
+                    case 2:
+                        if (source.Any())
+                        {
+                            source.Pop();
+                        }
+                        continue;
+                    case 3:
+                        if (source.Any())
+                        {
+                            Console.WriteLine(source.Max());
+                        }
+                        continue;
+                    case 4:
+                        if (source.Any())
+                        {
+                            Console.WriteLine(source.Min());
+                        }
+                        continue;
+                    default:
+                        break;
+
+                }
+            }
+            Console.WriteLine(string.Join(", ", (source)));
+        }
+    }
+}
