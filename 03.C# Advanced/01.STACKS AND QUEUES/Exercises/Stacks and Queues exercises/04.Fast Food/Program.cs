f95bf34123f2d6525e86d4c0f44740ad075d03d5
MarinBalashkov <Mkbalashkov@students.softuni.bg>
2020-02-03 16:57:46 +0200

    add Problem 4

---
+++ 03.C# Advanced/01.STACKS AND QUEUES/Exercises/Stacks and Queues exercises/04.Fast Food/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace _04.Fast_Food
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            int foodQuantity = int.Parse(Console.ReadLine());
+            var orders = new Queue<int>(Console.ReadLine().Split().Select(int.Parse).ToList());
+            int biggestOrder = orders.Max();
+
+            while (orders.Any())
+            {
+                int orderQuantity = orders.Peek();
+                if (foodQuantity >= orderQuantity)
+                {
+                    foodQuantity -= orderQuantity;
+                    orders.Dequeue();
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine(biggestOrder);
+
+            if (orders.Any())
+            {
+                Console.WriteLine($"Orders left: {string.Join(" ", (orders))}");
+            }
+            else
+            {
+                Console.WriteLine("Orders complete");
+            }
+        }
+    }
+}
