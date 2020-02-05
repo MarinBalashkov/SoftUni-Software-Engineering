701c1897e7ce86bdad15a608a93d3eb5e12488d4
MarinBalashkov <Mkbalashkov@students.softuni.bg>
2020-02-05 12:08:47 +0200

    Revert "dell some files"
    This reverts commit e8a59e599ab80cb5ef2941d2021fb4ce4eb1aeb5.

---
+++ 03.C# Advanced/01.STACKS AND QUEUES/Lab and lecture/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Queue<int> queue = new Queue<int>();
+            //queue.Enqueue(1);
+            //queue.Enqueue(2);
+
+            //var num = queue.Dequeue();
+
+            var allchildreen = Console.ReadLine().Split();
+            Queue<string> children = new Queue<string>(allchildreen);
+
+            while (children.Count > 1)
+            {
+                var currentChild = children.Dequeue();
+
+                if (true)
+                {
+
+                }
+            }
+        }
+    }
+}
