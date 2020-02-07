9031e2caa63cf686c750f94144f12743fae248d9
MarinBalashkov <Mkbalashkov@students.softuni.bg>
2020-02-07 15:12:04 +0200

    add Problem 4

---
+++ 03.C# Advanced/02.MULTIIDIMENSIONAL ARRAYS/Exercises/02.MULTIIDIMENSIONAL-ARRAYS-exercises/04. Matrix Shuffling/Program.cs
@@ -0,0 +1,88 @@
+﻿namespace _04._Matrix_Shuffling
+{
+    using System;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            int[] matrixSize = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int rows = matrixSize[0];
+            int cols = matrixSize[1];
+
+            string[,] matrix = new string[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                var currentRow = Console.ReadLine()
+                    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = currentRow[col];
+                }
+            }
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "END")
+                {
+                    break;
+                }
+
+                var tokens = input
+                    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                string command = tokens[0];
+
+                if (command != "swap" || tokens.Length < 5 || tokens.Length > 5)
+                {
+                    Console.WriteLine("Invalid input!");
+                    continue;
+                }
+                else
+                {
+                    int row1 = int.Parse(tokens[1]);
+                    int col1 = int.Parse(tokens[2]);
+                    int row2 = int.Parse(tokens[3]);
+                    int col2 = int.Parse(tokens[4]);
+
+                    if (row1 < 0 || row1 > matrix.GetLength(0) - 1
+                        || row2 < 0 || row2 > matrix.GetLength(0) - 1
+                        || col1 < 0 || col1 > matrix.GetLength(1) - 1
+                        || col2 < 0 || col2 > matrix.GetLength(1) - 1)
+                    {
+                        Console.WriteLine("Invalid input!");
+                        continue;
+                    }
+                    else
+                    {
+                        var firstElement = matrix[row1, col1];
+                        var seconsElement = matrix[row2, col2];
+                        matrix[row1, col1] = seconsElement;
+                        matrix[row2, col2] = firstElement;
+
+                        for (int row = 0; row < rows; row++)
+                        {
+                            for (int col = 0; col < cols; col++)
+                            {
+                                Console.Write(matrix[row, col] + " ");
+                            }
+
+                            Console.WriteLine();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
