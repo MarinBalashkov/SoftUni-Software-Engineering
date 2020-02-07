7faeb8a7dc526f2eed84bab827ba207652285011
MarinBalashkov <Mkbalashkov@students.softuni.bg>
2020-02-07 15:11:48 +0200

    add Problem 3

---
+++ 03.C# Advanced/02.MULTIIDIMENSIONAL ARRAYS/Exercises/02.MULTIIDIMENSIONAL-ARRAYS-exercises/03. Maximal Sum/Program.cs
@@ -0,0 +1,74 @@
+﻿namespace _03._Maximal_Sum
+{
+    using System;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            int[] matrixSize = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int rows = matrixSize[0];
+            int cols = matrixSize[1];
+
+            int[,] matrix = new int[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                var currentRow = Console.ReadLine()
+                    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = currentRow[col];
+                }
+            }
+
+            int maxSum = int.MinValue;
+            int startRowIndex = 0;
+            int startColIndex = 0;
+
+            for (int row = 0; row < rows - 2; row++)
+            {
+                for (int col = 0; col < cols - 2; col++)
+                {
+                    int currentSum = matrix[row, col]
+                        + matrix[row, col + 1]
+                        + matrix[row, col + 2]
+                        + matrix[row + 1, col]
+                        + matrix[row + 1, col + 1]
+                        + matrix[row + 1, col + 2]
+                        + matrix[row + 2, col]
+                        + matrix[row + 2, col + 1]
+                        + matrix[row + 2, col + 2];
+                    if (currentSum > maxSum)
+                    {
+                        maxSum = currentSum;
+                        startRowIndex = row;
+                        startColIndex = col;
+                    }
+
+
+                }
+            }
+
+            Console.WriteLine($"Sum = {maxSum}");
+
+            for (int row = startRowIndex; row < startRowIndex + 3; row++)
+            {
+                for (int col = startColIndex; col < startColIndex + 3; col++)
+                {
+                    Console.Write(matrix[row, col] + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
