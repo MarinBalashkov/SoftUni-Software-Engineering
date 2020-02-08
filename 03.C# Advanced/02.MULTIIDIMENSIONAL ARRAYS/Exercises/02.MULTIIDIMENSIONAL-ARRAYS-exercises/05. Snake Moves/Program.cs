ff101001b3f8cec297ef62aba865d494eef9a8c6
MarinBalashkov <Mkbalashkov@students.softuni.bg>
2020-02-08 15:12:30 +0200

    add Problem 5

---
+++ 03.C# Advanced/02.MULTIIDIMENSIONAL ARRAYS/Exercises/02.MULTIIDIMENSIONAL-ARRAYS-exercises/05. Snake Moves/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace _05._Snake_Moves
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            int[] matrixSize = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var snake = Console.ReadLine().ToCharArray();
+
+            int rows = matrixSize[0];
+            int cols = matrixSize[1];
+
+            char[,] matrix = new char[rows, cols];
+
+            Queue<char> snakeQueue = new Queue<char>(snake);
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    char charToAdd = snakeQueue.Dequeue();
+                    matrix[row, col] = charToAdd;
+                    snakeQueue.Enqueue(charToAdd);
+                }
+            }
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    Console.Write(matrix[row, col]);
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
