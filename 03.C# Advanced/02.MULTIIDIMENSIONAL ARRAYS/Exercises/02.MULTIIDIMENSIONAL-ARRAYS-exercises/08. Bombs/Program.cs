35381d7d1e075eb5ced03633f0aed5827d7e6d6f
MarinBalashkov <Mkbalashkov@students.softuni.bg>
2020-02-09 15:14:41 +0200

    add Problems 8 and 9

---
+++ 03.C# Advanced/02.MULTIIDIMENSIONAL ARRAYS/Exercises/02.MULTIIDIMENSIONAL-ARRAYS-exercises/08. Bombs/Program.cs
@@ -0,0 +1,293 @@
+﻿namespace _08._Bombs
+{
+using System;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[][] matrix = new int[n][];
+
+            for (int row = 0; row < n; row++)
+            {
+                var input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                matrix[row] = input;
+            }
+
+            var bombLocations = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            for (int i = 0; i < bombLocations.Length; i++)
+            {
+                var bombCoordinates = bombLocations[i].Split(',', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                int rowBomb = bombCoordinates[0];
+                int colBomb = bombCoordinates[1];
+                int damage = matrix[rowBomb][colBomb];
+                matrix[rowBomb][colBomb] = 0;
+
+                if (n == 1)
+                {
+                    continue;
+                }
+                else if (rowBomb > 0 && rowBomb < n - 1 && colBomb > 0 && colBomb < n - 1)
+                {
+                    if (matrix[rowBomb - 1][colBomb - 1] > 0)
+                    {
+                        matrix[rowBomb - 1][colBomb - 1] -= damage;
+                    }
+
+                    if (matrix[rowBomb - 1][colBomb] > 0)
+                    {
+                        matrix[rowBomb - 1][colBomb] -= damage;
+                    }
+
+                    if (matrix[rowBomb - 1][colBomb + 1] > 0)
+                    {
+                        matrix[rowBomb - 1][colBomb + 1] -= damage;
+                    }
+
+                    if (matrix[rowBomb][colBomb - 1] > 0)
+                    {
+                        matrix[rowBomb][colBomb - 1] -= damage;
+                    }
+
+                    if (matrix[rowBomb][colBomb + 1] > 0)
+                    {
+                        matrix[rowBomb][colBomb + 1] -= damage;
+                    }
+
+                    if (matrix[rowBomb + 1][colBomb - 1] > 0)
+                    {
+                        matrix[rowBomb + 1][colBomb - 1] -= damage;
+                    }
+
+                    if (matrix[rowBomb + 1][colBomb] > 0)
+                    {
+                        matrix[rowBomb + 1][colBomb] -= damage;
+                    }
+
+                    if (matrix[rowBomb + 1][colBomb + 1] > 0)
+                    {
+                        matrix[rowBomb + 1][colBomb + 1] -= damage;
+                    }
+                }
+                else if (rowBomb == 0 && colBomb > 0 && colBomb < n - 1)
+                {
+                    if (matrix[rowBomb][colBomb - 1] > 0)
+                    {
+                        matrix[rowBomb][colBomb - 1] -= damage;
+                    }
+
+                    if (matrix[rowBomb][colBomb + 1] > 0)
+                    {
+                        matrix[rowBomb][colBomb + 1] -= damage;
+                    }
+
+                    if (matrix[rowBomb + 1][colBomb - 1] > 0)
+                    {
+                        matrix[rowBomb + 1][colBomb - 1] -= damage;
+                    }
+
+                    if (matrix[rowBomb + 1][colBomb] > 0)
+                    {
+                        matrix[rowBomb + 1][colBomb] -= damage;
+                    }
+
+                    if (matrix[rowBomb + 1][colBomb + 1] > 0)
+                    {
+                        matrix[rowBomb + 1][colBomb + 1] -= damage;
+                    }
+                }
+                else if (rowBomb == 0 && colBomb == 0)
+                {
+
+                    if (matrix[rowBomb][colBomb + 1] > 0)
+                    {
+                        matrix[rowBomb][colBomb + 1] -= damage;
+                    }
+
+                    if (matrix[rowBomb + 1][colBomb] > 0)
+                    {
+                        matrix[rowBomb + 1][colBomb] -= damage;
+
+                    }
+
+                    if (matrix[rowBomb + 1][colBomb + 1] > 0)
+                    {
+                        matrix[rowBomb + 1][colBomb + 1] -= damage;
+
+                    }
+                }
+                else if (rowBomb == 0 && colBomb == n - 1)
+                {
+                    if (matrix[rowBomb][colBomb - 1] > 0)
+                    {
+                        matrix[rowBomb][colBomb - 1] -= damage;
+
+                    }
+
+                    if (matrix[rowBomb + 1][colBomb - 1] > 0)
+                    {
+                        matrix[rowBomb + 1][colBomb - 1] -= damage;
+                    }
+
+                    if (matrix[rowBomb + 1][colBomb] > 0)
+                    {
+                        matrix[rowBomb + 1][colBomb] -= damage;
+                    }
+                }
+                else if (rowBomb == n - 1 && colBomb > 0 && colBomb < n - 1)
+                {
+                    if (matrix[rowBomb - 1][colBomb - 1] > 0)
+                    {
+                        matrix[rowBomb - 1][colBomb - 1] -= damage;
+                    }
+
+                    if (matrix[rowBomb - 1][colBomb] > 0)
+                    {
+                        matrix[rowBomb - 1][colBomb] -= damage;
+                    }
+
+                    if (matrix[rowBomb - 1][colBomb + 1] > 0)
+                    {
+                        matrix[rowBomb - 1][colBomb + 1] -= damage;
+                    }
+
+                    if (matrix[rowBomb][colBomb - 1] > 0)
+                    {
+                        matrix[rowBomb][colBomb - 1] -= damage;
+
+                    }
+
+                    if (matrix[rowBomb][colBomb + 1] > 0)
+                    {
+                        matrix[rowBomb][colBomb + 1] -= damage;
+                    }
+                }
+                else if (rowBomb == n - 1 && colBomb == 0)
+                {
+
+                    if (matrix[rowBomb - 1][colBomb] > 0)
+                    {
+                        matrix[rowBomb - 1][colBomb] -= damage;
+                    }
+
+                    if (matrix[rowBomb - 1][colBomb + 1] > 0)
+                    {
+                        matrix[rowBomb - 1][colBomb + 1] -= damage;
+
+                    }
+
+                    if (matrix[rowBomb][colBomb + 1] > 0)
+                    {
+                        matrix[rowBomb][colBomb + 1] -= damage;
+                    }
+                }
+                else if (rowBomb == n - 1 && colBomb == n - 1)
+                {
+                    if (matrix[rowBomb - 1][colBomb - 1] > 0)
+                    {
+                        matrix[rowBomb - 1][colBomb - 1] -= damage;
+                    }
+
+                    if (matrix[rowBomb - 1][colBomb] > 0)
+                    {
+                        matrix[rowBomb - 1][colBomb] -= damage;
+
+                    }
+
+                    if (matrix[rowBomb][colBomb - 1] > 0)
+                    {
+                        matrix[rowBomb][colBomb - 1] -= damage;
+                    }
+                }
+                else if (rowBomb > 0 && rowBomb < n - 1 && colBomb == 0)
+                {
+                    if (matrix[rowBomb - 1][colBomb] > 0)
+                    {
+                        matrix[rowBomb - 1][colBomb] -= damage;
+
+                    }
+
+                    if (matrix[rowBomb - 1][colBomb + 1] > 0)
+                    {
+                        matrix[rowBomb - 1][colBomb + 1] -= damage;
+
+                    }
+
+                    if (matrix[rowBomb][colBomb + 1] > 0)
+                    {
+                        matrix[rowBomb][colBomb + 1] -= damage;
+
+                    }
+
+                    if (matrix[rowBomb + 1][colBomb] > 0)
+                    {
+                        matrix[rowBomb + 1][colBomb] -= damage;
+
+                    }
+
+                    if (matrix[rowBomb + 1][colBomb + 1] > 0)
+                    {
+                        matrix[rowBomb + 1][colBomb + 1] -= damage;
+
+                    }
+                }
+                else if (rowBomb > 0 && rowBomb < n - 1 && colBomb == n - 1)
+                {
+                    if (matrix[rowBomb - 1][colBomb - 1] > 0)
+                    {
+                        matrix[rowBomb - 1][colBomb - 1] -= damage;
+                    }
+
+                    if (matrix[rowBomb - 1][colBomb] > 0)
+                    {
+                        matrix[rowBomb - 1][colBomb] -= damage;
+
+                    }
+
+                    if (matrix[rowBomb][colBomb - 1] > 0)
+                    {
+                        matrix[rowBomb][colBomb - 1] -= damage;
+
+                    }
+
+                    if (matrix[rowBomb + 1][colBomb - 1] > 0)
+                    {
+                        matrix[rowBomb + 1][colBomb - 1] -= damage;
+
+                    }
+
+                    if (matrix[rowBomb + 1][colBomb] > 0)
+                    {
+                        matrix[rowBomb + 1][colBomb] -= damage;
+
+                    }
+                }
+            }
+
+            int count = 0;
+            int sum = 0;
+
+            foreach (var row in matrix)
+            {
+                foreach (var item in row)
+                {
+                    if (item > 0)
+                    {
+                        count++;
+                        sum += item;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Alive cells: {count}");
+            Console.WriteLine($"Sum: {sum}");
+            Console.WriteLine(string.Join(Environment.NewLine, matrix
+                .Select(r => string.Join(" ", r))));
+
+        }
+    }
+}
---
+++ 03.C# Advanced/02.MULTIIDIMENSIONAL ARRAYS/Exercises/02.MULTIIDIMENSIONAL-ARRAYS-exercises/09. Miner/Program.cs
@@ -0,0 +1,134 @@
+﻿namespace _09._Miner
+{
+    using System;
+    using System.Linq;
+
+    public class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            var commands = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            char[][] matrix = new char[n][];
+
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(char.Parse).ToArray();
+                matrix[i] = input;
+            }
+
+            int minerRowPosition = 0;
+            int minerColPosition = 0;
+            int coals = 0;
+
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    if (matrix[row][col] == 's')
+                    {
+                        minerRowPosition = row;
+                        minerColPosition = col;
+                    }
+                    else if (matrix[row][col] == 'c')
+                    {
+                        coals++;
+                    }
+                }
+            }
+
+
+            for (int i = 0; i < commands.Length; i++)
+            {
+                string command = commands[i];
+
+                if (command == "left" && minerColPosition - 1 >= 0)
+                {
+                    minerColPosition--;
+                    if (matrix[minerRowPosition][minerColPosition] == 'c')
+                    {
+                        coals--;
+                        matrix[minerRowPosition][minerColPosition] = '*';
+
+                        if (coals == 0)
+                        {
+                            Console.WriteLine($"You collected all coals! ({minerRowPosition}, {minerColPosition})");
+                            return;
+                        }
+                    }
+                    else if (matrix[minerRowPosition][minerColPosition] == 'e')
+                    {
+                        Console.WriteLine($"Game over! ({minerRowPosition}, {minerColPosition})");
+                        return;
+                    }
+
+                }
+                else if (command == "right" && minerColPosition + 1 <= n - 1)
+                {
+                    minerColPosition++;
+                    if (matrix[minerRowPosition][minerColPosition] == 'c')
+                    {
+                        coals--;
+                        matrix[minerRowPosition][minerColPosition] = '*';
+
+                        if (coals == 0)
+                        {
+                            Console.WriteLine($"You collected all coals! ({minerRowPosition}, {minerColPosition})");
+                            return;
+                        }
+                    }
+                    else if (matrix[minerRowPosition][minerColPosition] == 'e')
+                    {
+                        Console.WriteLine($"Game over! ({minerRowPosition}, {minerColPosition})");
+                        return;
+                    }
+
+                }
+                else if (command == "up" && minerRowPosition - 1 >= 0)
+                {
+                    minerRowPosition--;
+                    if (matrix[minerRowPosition][minerColPosition] == 'c')
+                    {
+                        coals--;
+                        matrix[minerRowPosition][minerColPosition] = '*';
+
+                        if (coals == 0)
+                        {
+                            Console.WriteLine($"You collected all coals! ({minerRowPosition}, {minerColPosition})");
+                            return;
+                        }
+                    }
+                    else if (matrix[minerRowPosition][minerColPosition] == 'e')
+                    {
+                        Console.WriteLine($"Game over! ({minerRowPosition}, {minerColPosition})");
+                        return;
+                    }
+
+                }
+                else if (command == "down" && minerRowPosition + 1 <= n - 1)
+                {
+                    minerRowPosition++;
+                    if (matrix[minerRowPosition][minerColPosition] == 'c')
+                    {
+                        coals--;
+                        matrix[minerRowPosition][minerColPosition] = '*';
+                        if (coals == 0)
+                        {
+                            Console.WriteLine($"You collected all coals! ({minerRowPosition}, {minerColPosition})");
+                            return;
+                        }
+                    }
+                    else if (matrix[minerRowPosition][minerColPosition] == 'e')
+                    {
+                        Console.WriteLine($"Game over! ({minerRowPosition}, {minerColPosition})");
+                        return;
+                    }
+
+                }
+            }
+
+            Console.WriteLine($"{coals} coals left. ({minerRowPosition}, {minerColPosition})");
+        }
+    }
+}
