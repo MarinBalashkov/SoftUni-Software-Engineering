b051e84e1d6fca11e278a165c21a67a5c3809836
MarinBalashkov <Mkbalashkov@students.softuni.bg>
2020-02-09 15:25:09 +0200

    Problem 6

---
+++ 03.C# Advanced/02.MULTIIDIMENSIONAL ARRAYS/Exercises/02.MULTIIDIMENSIONAL-ARRAYS-exercises/06. Bomb the Basement/Program.cs
@@ -0,0 +1,79 @@
+﻿namespace _06._Bomb_the_Basement
+{
+    using System;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] dimensions = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] bombParameters = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int targetRow = bombParameters[0];
+            int targetCol = bombParameters[1];
+            int radius = bombParameters[2];
+
+            int rows = dimensions[0];
+            int cols = dimensions[1];
+            int[,] matrix = new int[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = 0;
+                }
+            }
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    bool isInRadius = Math.Pow(row - targetRow, 2) + Math.Pow(col - targetCol, 2) <= Math.Pow(radius, 2);
+
+                    if (isInRadius)
+                    {
+                        matrix[row, col] = 1;
+                    }
+                }
+            }
+
+            for (int col = 0; col < cols; col++)
+            {
+                int counter = 0;
+
+                for (int row = 0; row < rows; row++)
+                {
+                    if (matrix[row, col] == 1)
+                    {
+                        counter++;
+                        matrix[row, col] = 0;
+                    }
+                }
+
+                for (int row = 0; row < counter; row++)
+                {
+                    matrix[row, col] = 1;
+                }
+            }
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    Console.Write(matrix[row, col]);
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
