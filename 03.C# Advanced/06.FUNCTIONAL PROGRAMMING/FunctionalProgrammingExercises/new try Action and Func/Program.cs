5d7bc42dc8f6433b52bf8aa6b6c24725fba2b488
MarinBalashkov <Mkbalashkov@students.softuni.bg>
2020-03-09 17:25:24 +0200

    add Functional Programming files

---
+++ 03.C# Advanced/06.FUNCTIONAL PROGRAMMING/FunctionalProgrammingExercises/new try Action and Func/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace new_try_Action_and_Func
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int[] bounds = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int lowerBound = bounds[0];
+            int upperBound = bounds[1];
+
+            string condition = Console.ReadLine();
+
+            List<int> numbers = new List<int>();
+            for (int i = lowerBound; i <= upperBound; i++)
+            {
+                numbers.Add(i);
+            }
+
+            Predicate<int> idOddOrEvven = OddOrEvven(condition);
+            Action<List<int>> print = numbers => Console.WriteLine(string.Join(" ", numbers.Where(x => idOddOrEvven(x))));
+
+            print(numbers);
+        }
+
+        public static Predicate<int> OddOrEvven(string condition)
+        {
+            if (condition == "odd")
+            {
+                return x => x % 2 != 0;
+            }
+            else
+            {
+                return x => x % 2 == 0;
+            }
+        }
+    }
+}
