16cccdfa2467638e25f272887a8c5b6ba8f04d92
MarinBalashkov <Mkbalashkov@students.softuni.bg>
2021-01-21 14:27:34 +0200

    add logging models

---
+++ 07.C# Web Basics/MyMVCFramework/SIS.HTTP/Logging/ConsoleLogger.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+
+namespace SIS.HTTP.Logging
+{
+    public class ConsoleLogger : ILogger
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine($"[{DateTime.Now.ToString()}] {message}");
+        }
+    }
+}
