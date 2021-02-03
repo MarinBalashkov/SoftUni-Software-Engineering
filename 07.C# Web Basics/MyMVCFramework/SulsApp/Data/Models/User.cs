69f8501451183741b704cede474280cd20a8caf7
MarinBalashkov <Mkbalashkov@students.softuni.bg>
2021-02-03 16:11:15 +0200

    add DB and Db models

---
+++ 07.C# Web Basics/MyMVCFramework/SulsApp/Data/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using SIS.MvcFramework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SulsApp.Data.Models
+{
+    public class User : IdentityUser<string>
+    {
+        public User()
+        {
+            this.Id = Guid.NewGuid().ToString();
+            this.Submissions = new HashSet<Submission>();
+        }
+
+        public virtual ICollection<Submission> Submissions { get; set; }
+    }
+}
--- 07.C# Web Basics/MyMVCFramework/SulsApp/Startup.cs
+++ 07.C# Web Basics/MyMVCFramework/SulsApp/Startup.cs
@@ -1,7 +1,9 @@
 ﻿namespace SulsApp
 {
+    using Microsoft.EntityFrameworkCore;
     using SIS.HTTP;
     using SIS.MvsFramework;
+    using SulsApp.Data;
     using System;
     using System.Collections.Generic;
     using System.Text;
@@ -9,12 +11,13 @@
     {
         public void Configure(IList<Route> routeTable)
         {
-            
+            var db = new ApplicationDbContext();
+            db.Database.Migrate();
         }
 
         public void ConfigureServices()
         {
-            throw new NotImplementedException();
+
         }
     }
 }
