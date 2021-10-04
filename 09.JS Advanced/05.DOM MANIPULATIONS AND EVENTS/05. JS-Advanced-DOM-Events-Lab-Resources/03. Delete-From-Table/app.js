9ad2a2f8961e22f710d5eff311beea9f5a63ce20
MarinBalashkov <Mkbalashkov@students.softuni.bg>
2021-10-04 17:32:40 +0300

    Add DOM Manipulation Lab 1, 2 , 3

---
+++ 09.JS Advanced/05.DOM MANIPULATIONS AND EVENTS/05. JS-Advanced-DOM-Events-Lab-Resources/03. Delete-From-Table/app.js
@@ -0,0 +1,22 @@
+function deleteByEmail() {
+    const email = document.querySelector('input[name="email]').value;
+
+    const rows = Array.from(document.querySelectorAll('tbody tr'))
+
+    let deleted = false;
+
+    for (const row of rows) {
+        if (row.children[1] === email) {
+            row.parentNode.removeChild(row);
+            deleted = true;
+            document.getElementById('result').textContent = 'Deleted.'
+        }
+    }
+
+    if (deleted) {
+        document.getElementById('result').textContent = 'Not found.';
+    }
+
+
+
+}
