fe6611d55d3eb39e6e8e0b7a09e86cd5955cd627
MarinBalashkov <Mkbalashkov@students.softuni.bg>
2021-10-12 23:02:28 +0300

    lab 1, 2

---
+++ 09.JS Advanced/06.ADVANCED FUNCTIONS/Lab/01.js
@@ -0,0 +1,25 @@
+function solve(area, vol, data) {
+    return JSON.parse(data).map((obj) => ({
+        area: area.call(obj),
+        volume: vol.call(obj)
+    }))
+}
+
+
+function area() {
+    return Math.abs(this.x * this.y);
+}
+
+function vol() {
+    return Math.abs(this.x * this.y * this.z);
+}
+
+solve(
+    area,
+    vol,
+    `[
+    {"x":"1","y":"2","z":"10"},
+    {"x":"7","y":"7","z":"10"},
+    {"x":"5","y":"2","z":"10"}
+    ]`
+);
