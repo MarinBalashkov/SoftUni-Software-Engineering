8ba5ee3d122fe6b0707a044233432109fe91f01a
MarinBalashkov <Mkbalashkov@students.softuni.bg>
2021-10-19 23:06:42 +0300

    exercise 3

---
+++ 09.JS Advanced/06.ADVANCED FUNCTIONS/06. JS-Advanced-Advanced-Functions-Exercise-Resources/03.js
@@ -0,0 +1,21 @@
+
+function fibonacci() {
+    let [a, b] = [0, 1];
+    let c;
+
+    return function () {
+        c = a + b;
+        a = b;
+        b = c;
+        return a;
+    };
+}
+
+const fib = fibonacci();
+console.log(fib()); // 1
+console.log(fib()); // 1
+console.log(fib()); // 2
+console.log(fib()); // 3
+console.log(fib()); // 5
+console.log(fib()); // 8
+console.log(fib()); // 13
