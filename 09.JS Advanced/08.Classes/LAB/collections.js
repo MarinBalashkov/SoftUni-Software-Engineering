aea7d8e307e71758b5bd14914d85b322ae7264e4
MarinBalashkov <Mkbalashkov@students.softuni.bg>
2021-11-13 23:22:32 +0200

    lab collections.js

---
+++ 09.JS Advanced/08.Classes/LAB/collections.js
@@ -0,0 +1,18 @@
+const myMap = new Map();
+
+myMap.set('Peter', '+1-555-5255-1421');
+myMap.set('Jhon', '+1-555-5255-1421');
+
+//console.log(myMap);
+//console.log(myMap.get('Peter'));
+myMap.set('Peter', '+325-453541-5455')
+
+
+
+
+console.log(Array.from(myMap));
+
+
+for (const [key, vaue] of myMap) {
+    
+}
