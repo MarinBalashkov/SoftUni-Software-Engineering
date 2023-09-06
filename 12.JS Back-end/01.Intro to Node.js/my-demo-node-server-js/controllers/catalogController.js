4dc0469f9509cbe73f52be14b2467a3e5bab5a5f
MarinBalashkov <Mkbalashkov@students.softuni.bg>
2023-09-06 14:47:53 +0300

    listing items in catalog

--- 12.JS Back-end/01.Intro to Node.js/my-demo-node-server-js/controllers/catalogController.js
+++ 12.JS Back-end/01.Intro to Node.js/my-demo-node-server-js/controllers/catalogController.js
@@ -1,7 +1,10 @@
+const { database } = require('../util/database');
 const { layout, render, loadTemplate } = require('../util/template');
 
 module.exports = async (req, res) => {
-    const catalogPage = await loadTemplate('catalog');
+    const catalogPage = await render('catalog', {
+        items: Object.entries(database).map(([id, i]) => `<li data-id="${id}">${i.name} - ${i.serial} <a href="/delete?id=${id}">[Delete]</a></li>`).join('')
+    });
     res.write(await layout(catalogPage, 'Catalog'));
     res.end();
 };
