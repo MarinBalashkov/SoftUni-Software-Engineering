a19c96484eb44024a5dde8fb8317188ebc128906
MarinBalashkov <Mkbalashkov@students.softuni.bg>
2023-10-01 15:37:59 +0300

    add details page!

--- 12.JS Back-end/worksop-cubicle/controllers/details.js
+++ 12.JS Back-end/worksop-cubicle/controllers/details.js
@@ -1,5 +1,14 @@
 module.exports = {
-    details(req, res) {
-        res.render('details');
+    async details(req, res) {
+        try {
+            const cube =  await req.storage.getById(req.params.id);
+            if (cube == undefined) {
+                res.redirect('404');
+            }
+
+            res.render('details', cube);
+        } catch (error) {
+            console.error(error.message);
+        }
     }
 };
